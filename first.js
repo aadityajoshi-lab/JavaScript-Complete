3c9bb951527a484ffffe0e0eb6149c1ab72830ce
aadityajoshi-lab <itsmeaadityajoshi.com>
2025-11-29 14:05:58 +0545

    first commit

---
+++ first.js
@@ -0,0 +1,17 @@
+let y = undefined;
+let x = null;
+let age = 10;
+let name = "Aaditya Joshi";
+let isFollow = true;
+
+
+console.log(name);
+
+const student = {
+    name: "Aaditya Joshi",
+    age: 25,
+    cgpa: 8
+};
+student.age = 50
+console.log(student["age"]);
+console.log(student.age);
