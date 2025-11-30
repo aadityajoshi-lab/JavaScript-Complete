6f062cea63c856faae67287e4e0efaa00940d6db
aadityajoshi-lab <itsmeaadityajoshi.com>
2025-11-30 21:46:35 +0545

    practice

--- first.js
+++ first.js
@@ -179,17 +179,17 @@
 
 // companies = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
 
-// console.log(companies);
+console.log(companies);
 
-// companies.shift();
+companies.shift();
 
-// console.log(companies);
+console.log(companies);
 
-// companies.splice(1,1,"Ola");
+companies.splice(1,1,"Ola");
 
-// console.log(companies);
+console.log(companies);
 
-// companies.push("Amazon");
+companies.push("Amazon");
 
-// console.log(companies);
+console.log(companies);
 
